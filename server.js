01ecb3b5c9eee62c186e29921bc7a56caf50e99b
VIKAS Neriyanuru <vikasneriyanuru@VIKASs-MacBook-Pro.local>
2025-02-23 04:38:05 -0500

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const apiRoutes = require("./routes/index");
 app.use("/api", apiRoutes);
